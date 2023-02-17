37f0b6b76fc9f6567e3faa18581abc6803f71c14
amel83 <tesfaneshk222@gmail.com>
2023-02-16 16:43:20 -0800

    dj

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - use printf to print
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	Return(0);
+}
