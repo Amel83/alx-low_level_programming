8c483e7c601e10414b69528c9c2659883f912925
amel83 <tesfaneshk223@gmail.com>
2023-05-09 02:21:03 +0000

    c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -56,10 +56,17 @@ void print_class(unsigned char *e_ident)
 
 	switch (e_ident[EI_CLASS])
 	{
-	case ELFCLASSNONE: printf("none\n"); break;
-	case ELFCLASS32: printf("ELF32\n"); break;
-	case ELFCLASS64: printf("ELF64\n"); break;
-	default: printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	case ELFCLASSNONE: 
+		printf("none\n");
+	       	break;
+	case ELFCLASS32: 
+		printf("ELF32\n");
+	       	break;
+	case ELFCLASS64: 
+		printf("ELF64\n");
+	       	break;
+	default:
+	       	printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
 }
 
@@ -67,10 +74,17 @@ void print_data(unsigned char *e_ident) {
 	printf("  Data:                              ");
 	switch (e_ident[EI_DATA])
 	{
-	case ELFDATANONE: printf("none\n"); break;
-	case ELFDATA2LSB: printf("2's complement, little endian\n"); break;
-	case ELFDATA2MSB: printf("2's complement, big endian\n"); break;
-	default: printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	case ELFDATANONE: 
+		printf("none\n"); 
+		break;
+	case ELFDATA2LSB: 
+		printf("2's complement, little endian\n");
+	       	break;
+	case ELFDATA2MSB:
+	       	printf("2's complement, big endian\n");
+	       	break;
+	default:
+	       	printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
 }
 
@@ -138,12 +152,23 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 			e_type >>= 8;
 	printf("  Type:                              ");
 	switch (e_type) {
-	case ET_NONE: printf("NONE (None)\n"); break;
-	case ET_REL: printf("REL (Relocatable file)\n"); break;
-	case ET_EXEC: printf("EXEC (Executable file)\n"); break;
-	case ET_DYN: printf("DYN (Shared object file)\n"); break;
-	case ET_CORE: printf("CORE (Core file)\n"); break;
-	default: printf("<unknown: %x>\n", e_type);
+	case ET_NONE:
+	       	printf("NONE (None)\n");
+	       	break;
+	case ET_REL:
+	       	printf("REL (Relocatable file)\n"); 
+		break;
+	case ET_EXEC: 
+		printf("EXEC (Executable file)\n");
+	       	break;
+	case ET_DYN:
+	       	printf("DYN (Shared object file)\n");
+	       	break;
+	case ET_CORE:
+	       	printf("CORE (Core file)\n"); 
+		break;
+	default: 
+		printf("<unknown: %x>\n", e_type);
 	}
 }
 
