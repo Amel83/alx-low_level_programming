4fe1a3c6e32abfc4ec7cc9ad15ac586888bea327
root <amel83>
2023-06-16 12:32:21 -0700

    sj

---
+++ 0x17-doubly_linked_lists/work.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+largest_palindrome = 0
+
+for i in range(999, 99, -1):
+    for j in range(i, 99, -1):
+        product = i * j
+        product_str = str(product)
+        if product_str == product_str[::-1] and product > largest_palindrome:
+            largest_palindrome = product
+
+# Save the result in the file
+with open('102-result', 'w') as file:
+    file.write(str(largest_palindrome))
+
