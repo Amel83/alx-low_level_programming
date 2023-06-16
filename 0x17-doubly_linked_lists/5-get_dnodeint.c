6f9508bcc8ee1c76b3e625181249f5623bee0cdb
root <amel83>
2023-06-15 19:03:01 -0700

    fs

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,8 @@
  * get_dnodeint_at_index - adds a node at index
  * @head: points to the first list
  * @index: positiin to add the new node
- * Return: If fail NULL on success head */
+ * Return: If fail NULL on success head
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	for (; index != 0; index--)
