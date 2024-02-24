963a7c740343544d412d3378850bfbf4458eb01b
Amel83 <122824635+Amel83@users.noreply.github.com>
2024-02-24 02:06:30 -0800

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,29 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-  * reverse_array - Reverses the content of an array of integers
-  * @a: An array of integers
-  * @n: Number of elements to swap
-  *
-  * Return: empty
-  */
+ * reverse_array - Reverses the content of an array of integers
+ * @a: Pointer to the array
+ * @n: Number of elements in the array
+ */
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
+    int start = 0;
+    int end = n - 1;
+    int temp;
 
-	p = a;
+    while (start < end)
+    {
+        // Swap elements at start and end indices
+        temp = a[start];
+        a[start] = a[end];
+        a[end] = temp;
 
-	for (i = 1; i < n; i++)
-	{
-		p++;
-	}
-
-	for (k = 0; k < i / 2; k++)
-	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
-	}
+        // Move indices towards the center
+        start++;
+        end--;
+    }
 }
+
